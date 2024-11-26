5ef8636f4f9456c1e502f7768f3733de636d8684
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-26 15:40:33 +0100

    feat:adding findall particenpent

--- evenements-sportifs/src/participant/participant.service.ts
+++ evenements-sportifs/src/participant/participant.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateParticipantDto } from './dto/create-participant.dto';
 import { UpdateParticipantDto } from './dto/update-participant.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Participant } from './entities/participant.entity';
+import { Model } from 'mongoose';
+import { promises } from 'dns';
+import { exec } from 'child_process';
 
 @Injectable()
 export class ParticipantService {
-  create(createParticipantDto: CreateParticipantDto) {
-    return 'This action adds a new participant';
+  constructor(
+    @InjectModel(Participant.name) private participantModel: Model<Participant>,
+  ) {}
+
+  async createParticipant(
+    createParticipantDto: CreateParticipantDto,
+  ): Promise<Participant> {
+    const { email } = createParticipantDto;
+    const existParticipant = await this.participantModel
+      .findOne({ email }).exec()
+    if (existParticipant) {
+      throw new ConflictException(`a participent with ${email} already exists`);
+    }
+    const newParticipant = new this.participantModel(createParticipantDto);
+    return newParticipant.save();
   }
 
-  findAll() {
-    return `This action returns all participant`;
+  async findAllParticipant() {
+    return await this.participantModel.find() ;
   }
 
   findOne(id: number) {
