b8ab90a5735bdf593d7690ddcb3f6a6d4ab0ea23
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-26 15:35:52 +0100

    feat: adding feature for storing participent

--- evenements-sportifs/src/participant/participant.controller.ts
+++ evenements-sportifs/src/participant/participant.controller.ts
@@ -3,18 +3,20 @@ import { ParticipantService } from './participant.service';
 import { CreateParticipantDto } from './dto/create-participant.dto';
 import { UpdateParticipantDto } from './dto/update-participant.dto';
 
+
+
 @Controller('participant')
 export class ParticipantController {
   constructor(private readonly participantService: ParticipantService) {}
 
   @Post()
-  create(@Body() createParticipantDto: CreateParticipantDto) {
-    return this.participantService.create(createParticipantDto);
+  createParticipant(@Body() createParticipantDto: CreateParticipantDto) {
+    return this.participantService.createParticipant(createParticipantDto);
   }
 
   @Get()
-  findAll() {
-    return this.participantService.findAll();
+  findAllParticipant() {
+    return this.participantService.findAllParticipant();
   }
 
   @Get(':id')
