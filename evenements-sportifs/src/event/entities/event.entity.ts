bdacbf5008e0f5de9a93892e9aaea89aaec24f0a
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-26 22:32:53 +0100

    feat:adding entitie for event

--- evenements-sportifs/src/event/entities/event.entity.ts
+++ evenements-sportifs/src/event/entities/event.entity.ts
@@ -1,1 +1,23 @@
-export class Event {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { IsDate, IsString } from "class-validator";
+import { Date, Types } from "mongoose";
+
+@Schema({timestamps:true})
+export class Event {
+    @Prop({required:true})
+    name:string
+
+    @Prop({required:true})
+    description:string
+
+    @Prop({required:true})
+    image:string
+
+    @Prop({type:[{type:Types.ObjectId, ref:'Participant'}]})
+    participants: Types.ObjectId[]
+
+    @Prop({required:true})
+    date:Date
+
+}
+export const EventSchema = SchemaFactory.createForClass(Event)
