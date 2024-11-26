12b53ec3f577a82c9717332e37d903e302b0fbb3
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-26 14:17:52 +0100

    merging ES-27-auth branch with main

---
+++ evenements-sportifs/src/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request)
+    if(!token){
+        throw new UnauthorizedException('access denied, invalid token')
+    }
+    try {
+        const payload = this.jwtService.verify(token)
+        request.userId = payload.userId
+    } catch (err) {
+        Logger.error(err.message)
+        throw new UnauthorizedException("access denied, invalid token")
+    }
+    return true;
+  }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    return request.headers.authorization?.split(' ')[1];
+  }
+}
