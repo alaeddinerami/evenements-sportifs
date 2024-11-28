a3b02526216c8563cd54aeb79f7f7f969670a635
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-28 15:20:20 +0100

    fix: update App.jsx for frontend application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/auth/login";
 import Register from "./components/auth/Register";
+import Dashboard from "./pages/Dashboard";
+import AuthHelper from "./helpers/authHelper";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={ <AuthHelper> <Dashboard /> </AuthHelper> } />
       </Routes>
     </>
   );
