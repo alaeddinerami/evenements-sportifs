f6f6ac20ba5d11882db2fffd69a2bf1d5fc2d536
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-28 15:20:17 +0100

    fix: update main.ts for backend application

--- evenements-sportifs/src/main.ts
+++ evenements-sportifs/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,13 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true ,
   }))
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
