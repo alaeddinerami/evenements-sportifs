ababa8d2fd7dc72d37251299d543c9bcce31c3b3
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-28 17:22:30 +0100

    fix: update App.jsx for application layout and logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./components/auth/login";
 import Register from "./components/auth/Register";
 import Dashboard from "./pages/Dashboard";
 import AuthHelper from "./helpers/authHelper";
+import Participant from "./pages/Participant";
+import Event from "./pages/Event";
 
 function App() {
   return (
@@ -11,7 +13,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={ <AuthHelper> <Dashboard /> </AuthHelper> } />
+        <Route path="/dashboard" element={ <AuthHelper> <Dashboard /> </AuthHelper> } >
+
+       <Route index element={<Participant/>} />
+       <Route  path="/dashboard/events" element={<Event />} />
+        </Route>
+     
       </Routes>
     </>
   );
