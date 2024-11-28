2aafc40f2bef413fe4387f389bbaf987f930d315
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-28 17:22:41 +0100

    feat: add Participant page for participant details and management

---
+++ frontend/src/pages/Participant.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ParticipantTable from '../components/ParticipantTable'
+
+export default function Participant() {
+  return (
+    
+    <div><ParticipantTable/></div>
+  )
+}
